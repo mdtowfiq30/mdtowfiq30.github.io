20b8e78831e84cc3652dc5afb5a74f0958a25ba6
mdtowfiq30 <166207765+mdtowfiq30@users.noreply.github.com>
2025-03-02 18:32:52 +0600

    Update app.py

--- Safety_shoe/app.py
+++ Safety_shoe/app.py
@@ -25,7 +25,6 @@ st.markdown(
         text-align: center;
         color: #2c3e50;
         font-size: 2rem;
-        margin-bottom: 20px;
     }
     
     /* Subtitle Styling */
@@ -33,7 +32,6 @@ st.markdown(
         text-align: center;
         color: #2c3e50;
         font-size: 1.5rem;
-        margin-bottom: 20px;
     }
 
     /* Input Box Styling */
@@ -52,7 +50,6 @@ st.markdown(
         border: none;
         cursor: pointer;
         transition: 0.3s;
-        width: 100%; /* Make the button take full width on small screens */
     }
     .stButton > button:hover {
         background-color: #2980b9;
@@ -77,13 +74,6 @@ st.markdown(
         }
         .stTextInput > div > div > input {
             font-size: 16px;
-        }
-
-        /* Center the submit button and make it take full width */
-        .stButton {
-            width: 100%;
-            display: flex;
-            justify-content: center;
         }
     }
     
