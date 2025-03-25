740c44439a064aea044d03d97939c4b33e9f7b9c
mdtowfiq30 <166207765+mdtowfiq30@users.noreply.github.com>
2025-03-25 17:32:50 +0600

    Delete jmi_ohs/Edu_Hub/script.js

--- jmi_ohs/Edu_Hub/script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("pdf-select").addEventListener("change", function() {
-    let pdfFile = this.value;
-    document.getElementById("pdf-viewer").src = pdfFile;
-    document.getElementById("mobile-pdf-link").href = pdfFile;
-});
